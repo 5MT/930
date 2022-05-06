b007612fd93d1f0e2a319e27c84901c490087c31
5MT <run5mt_ms@hotmail.com>
2022-05-06 15:29:55 +0900

    計算ロジックまで

--- calculationDrillA4p.js
+++ calculationDrillA4p.js
@@ -1,12 +1,17 @@
 ﻿var iNmuberOfQuestions = 3;
+var iIndexOfElmOf1stNumber = 0;
+var iIndexOfElmOf2ndNumber = 2;
 //
 //	ロード時
 //
 function body_OnLoad() {
  // 現在の年を取得してデフォルト入力
  var iNumberOfDigit = 4;
+ document.getElementById("secAnswerMultiplication").innerText="";
+ document.getElementById("secAnswerDivision").innerText="";
  document.getElementById("tbx_NumberOfDigit").value = iNumberOfDigit;
- populateDrillQuestions(iNumberOfDigit);
+ populateDrillQandA(iNumberOfDigit);
+
  return;
 }
 //
@@ -14,101 +19,69 @@ function body_OnLoad() {
 //
 function tbx_NumberOfDigit_Enter(){
  if (event.keyCode == 13){
+  document.getElementById("secAnswerMultiplication").innerText="";
+  document.getElementById("secAnswerDivision").innerText="";
   var iNumberOfDigit = document.getElementById("tbx_NumberOfDigit").value;
-  populateDrillQuestions(iNumberOfDigit);
+  populateDrillQandA(iNumberOfDigit);
  }
  return;
 }
 
 
-function fillNumbers(elmOfOl){
- if(elmOfOl.tagName == "LI"){
-  elmOfOl.childNodes[0].innerText = Math.floor(Math.random()*Math.pow(10,iNumberOfDigit));
-  elmOfOl.childNodes[2].innerText = 4567890;
-  document.getElementById("secBottom").innerText = elmOfOl.childNodes[2].tagName;
-}
-}
-//
-//	表作成
-//
-function populateDrillQuestions(iNumberOfDigit) {
-  // まず掛け算部分について
-  var olMultiplication = document.getElementById("olMultiplication");
-  olMultiplication.childNodes.forEach(fillNumbers);
 
+// 紙面作成
+//
+function populateDrillQandA(iNumberOfDigit){
+  // メインフロー
+  fillQuesionNumbers(iNumberOfDigit).then(
+   // 成功 (resolved) フロー
+   ()=>{
+    // 答えを入れるフローはここから
+    var putAnswersMultiplication = (elmOfOl)=>{
+      if(elmOfOl.tagName == "LI"){
+        document.getElementById("secAnswerMultiplication").innerText
+        += Number(elmOfOl.childNodes[iIndexOfElmOf1stNumber].innerText)*Number(elmOfOl.childNodes[iIndexOfElmOf2ndNumber].innerText)
+        + ", ";
+      }
+    };
+    var putAnswersDivision = (elmOfOl)=>{
+      if(elmOfOl.tagName == "LI"){
+        document.getElementById("secAnswerDivision").innerText
+        += Math.round(Number(elmOfOl.childNodes[iIndexOfElmOf1stNumber].innerText)/Number(elmOfOl.childNodes[iIndexOfElmOf2ndNumber].innerText)*10000)/10000
+        + ", ";
+      }
+    };
+    document.getElementById("olMultiplication").childNodes.forEach(putAnswersMultiplication);
+    document.getElementById("olDivision").childNodes.forEach(putAnswersDivision);
+   }
+   ,
+   // 失敗 (rejected) フロー
+   ()=>{}
+  );
+
+  /* Promise */ function fillQuesionNumbers(iNumberOfDigit){
+    return new Promise((resolveFillQnum, rejectFillQNum)=>{
+      
+      // ol の子アイテム (li を期待している) をぞれぞれ引数にとって処理する。
+      // ol の子アイテムはブランクも含まれるので注意。
+      var fillNumbers = (elmOfOl)=>{
+        if(elmOfOl.tagName == "LI"){
+          elmOfOl.childNodes[iIndexOfElmOf1stNumber].innerText = Math.floor(Math.random()*Math.pow(10,iNumberOfDigit));
+          elmOfOl.childNodes[iIndexOfElmOf2ndNumber].innerText = Math.floor(Math.random()*Math.pow(10,iNumberOfDigit));
+        }
+      };
+       
+      // fillQuestionNumbers の本処理
+      document.getElementById("olMultiplication").childNodes.forEach(fillNumbers);
+      document.getElementById("olDivision").childNodes.forEach(fillNumbers);
+
+      // ちゃんと上の本処理が終わってからここに来てくれるといいが……
+      resolveFillQnum();
+    });
+  };
+}
   // デバッグ用
 //  document.getElementById("secBottom").innerText = olMultiplication.childNodes.length;
-
-  olMultiplication.childNodes.forEach(fillNumbers);
-  
-//
-// }
-  var spnQ1Num1 = document.getElementById("q1num1");
-  spnQ1Num1.innerText = Math.floor(Math.random()*Math.pow(10,iNumberOfDigit));
-  var spnQ1Num2 = document.getElementById("q1num2");
-  spnQ1Num2.innerText = Math.floor(Math.random()*Math.pow(10,iNumberOfDigit));
-
-
- var divPrint = document.getElementById("divPrint");
- 
- // 一旦画面上の印刷用 div 内を全てクリア
- while(divPrint.firstChild){
-  divPrint.removeChild(divPrint.firstChild);
- }
- 
- for(var iPage = 1; iPage <= 6; iPage++){
-  var secNthPage = document.getElementById("secTemplate").cloneNode(true);
-  secNthPage.id = "secPage" + iPage;
-  secNthPage.childNodes[1].id = `secMonth${iPage*2-1}`;
-  secNthPage.childNodes[3].id = `secMonth${iPage*2}`;
-  // カレンダーに日付を記入
-  fillCalendar(secNthPage.childNodes[1], iYear, iPage*2-1);
-  fillCalendar(secNthPage.childNodes[3], iYear, iPage*2);
-  // メモ欄を作成
-  createMemoField(secNthPage.childNodes[1]);
-  createMemoField(secNthPage.childNodes[3]);
-
-  // ページセクションをアペンド
-  divPrint.appendChild(secNthPage);
-//  if(iPage%2==0 && iPage != 6){
-//  }
- }
- return;
-}
-
-//
-//	カレンダーに月、日付を記入する
-//
-function fillCalendar(secNthMonth, iYear, iMonth){
-
- // 月ヘッダの入力と位置合わせ
- // secNthMonth.childNodes[1] は h1。
- secNthMonth.childNodes[1].innerText = iMonth;
- secNthMonth.childNodes[1].style.marginTop = (7 + 7.3*(iMonth-1)) + "mm";
- 
- var dFirst = new Date(iYear,iMonth-1,1);
- var dLast = (iMonth==12)? new Date(iYear+1,0,0): new Date(iYear,iMonth,0);
- // node に tbody を設定
- var tbCalendar = secNthMonth.childNodes[3].childNodes[3];
-  
- var iDayNum = 1;
- for (var iRow = 1; iRow <= 6; iRow++){
-  for (var iCol = 1; iCol <=7; iCol++){
-   // 範囲に入っていたら日付カウントを入力、休日なら休日マークをつける
-   if(dFirst.getDay() <= 7*(iRow - 1)+iCol-1 && 7*(iRow - 1)+iCol-1 <= dFirst.getDay()+dLast.getDate()-1){
-    // 日付
-    // ここは結構トリッキー。td や tr との間のホワイトスペースを子要素カウントしているのでこの式になる。
-    tbCalendar.childNodes[2*iRow-1].childNodes[2*iCol-1].innerText = iDayNum;
-    // 休日マーク
-    if (iCol == 1 || iCol==7) {
-     tbCalendar.childNodes[2*iRow-1].childNodes[2*iCol-1].className += " Holiday";
-    }
-    iDayNum++;
-   }
-  }
- }
- return;
-}
 
 //
 //	メモフィールドの作成
